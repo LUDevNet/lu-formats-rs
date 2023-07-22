1d4ce9ef4d59eebf54c9ddcd39fba1ee5c4a6f38
Xiphoseer <me@xiphoseer.de>
2023-07-22 11:59:27 +0200

    test: boundary_info

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,12 @@ include!(concat!(env!("OUT_DIR"), "/lib.rs"));
 
 #[cfg(test)]
 mod tests {
-    use crate::common::{
-        parse_quaternion, parse_u1_color, parse_u1_str, parse_u4_str, Quaternion, U1Color, U1Str,
-        U4Str,
+    use crate::{
+        common::{
+            parse_quaternion, parse_u1_color, parse_u1_str, parse_u4_str, Quaternion, U1Color,
+            U1Str, U4Str,
+        },
+        files::luz::{parse_boundary_info, BoundaryInfo},
     };
 
     use super::common::{parse_vector3, Vector3};
@@ -95,6 +98,46 @@ mod tests {
                 U4Str {
                     length: 3,
                     str: b"ABC"
+                }
+            ))
+        )
+    }
+
+    #[test]
+    fn test_boundary_info() {
+        let mut bytes = Vec::<u8>::new();
+        let vec = Vector3 {
+            x: 0.5,
+            y: 100.0,
+            z: 7.0,
+        };
+        // normal
+        bytes.extend(&0.5_f32.to_le_bytes());
+        bytes.extend(&100_f32.to_le_bytes());
+        bytes.extend(&7_f32.to_le_bytes());
+        // point
+        bytes.extend(&0.5_f32.to_le_bytes());
+        bytes.extend(&100_f32.to_le_bytes());
+        bytes.extend(&7_f32.to_le_bytes());
+        // dest_zone_id
+        bytes.extend(&1200_u32.to_le_bytes());
+        // dest_scene_id
+        bytes.extend(&10_u32.to_le_bytes());
+        // spawn_loc
+        bytes.extend(&0.5_f32.to_le_bytes());
+        bytes.extend(&100_f32.to_le_bytes());
+        bytes.extend(&7_f32.to_le_bytes());
+
+        assert_eq!(
+            parse_boundary_info(&bytes).finish(),
+            Ok((
+                EMPTY,
+                BoundaryInfo {
+                    normal: vec.clone(),
+                    point: vec.clone(),
+                    dest_zone_id: 1200,
+                    dest_scene_id: 10,
+                    spawn_loc: vec.clone()
                 }
             ))
         )
